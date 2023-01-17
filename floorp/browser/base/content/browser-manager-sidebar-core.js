97b2b01b125076d835ff40baf59d0c8f0a14909d
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2023-01-18 00:06:33 +0900

    add old branch file

---
+++ floorp/browser/base/content/browser-manager-sidebar-core.js
@@ -0,0 +1,301 @@
+const BROWSER_SIDEBAR_DATA_PREF = "floorp.browser.sidebar2.data";
+
+const STATIC_SIDEBARS = {
+    "floorp//bmt": {
+        "l10n": "show-browser-manager-sidebar",
+        "src": "chrome://browser/content/places/places.xhtml",
+        "privileged": true,
+    },
+    "floorp//bookmarks": {
+        "l10n": "show-bookmark-sidebar",
+        "src": "chrome://browser/content/places/bookmarksSidebar.xhtml",
+        "privileged": true,
+    },
+    "floorp//history": {
+        "l10n": "show-history-sidebar",
+        "src": "chrome://browser/content/places/historySidebar.xhtml",
+        "privileged": true,
+    },
+    "floorp//downloads": {
+        "l10n": "show-download-sidebar",
+        "src": "about:downloads",
+        "privileged": true,
+    },
+    "floorp//tst": {
+        "l10n": "show-TST-sidebar",
+        "src": getTSTsrc(),
+        "privileged": true,
+    }
+}
+
+let BROWSER_SIDEBAR_DATA = null;
+let BROWSER_SIDEBAR_IDS = null;
+
+function getTSTsrc() {
+    let addon_uuids = Services.prefs.getStringPref("extensions.webextensions.uuids");
+    let TST_addon_uuids = JSON.parse(addon_uuids)["treestyletab@piro.sakura.ne.jp"];
+    let sidebarURL = `moz-extension://${TST_addon_uuids}/sidebar/sidebar.html`;
+    return sidebarURL;
+}
+
+function readBMSData() {
+    let bms_default_data = Services.prefs.getDefaultBranch(null).getStringPref(BROWSER_SIDEBAR_DATA_PREF);
+    let init_data = JSON.parse(Services.prefs.getStringPref(BROWSER_SIDEBAR_DATA_PREF, "{}"));
+    if (typeof init_data["data"] !== "object") {
+        init_data = JSON.parse(bms_default_data);
+    }
+
+    // データの移行
+    // v1: floorp.browser.sidebar2.customurl*  floorp.browser.sidebar2.customurl*.usercontext
+    // v2: indexプロパティが存在する
+    function getPref(name) {
+        switch (Services.prefs.getPrefType(name)) {
+            case 0:
+              return undefined;
+            case 32:
+              return Services.prefs.getStringPref(name);
+            case 64:
+              return Services.prefs.getIntPref(name);
+            case 128:
+              return Services.prefs.getBoolPref(name);
+        }
+    }
+    for (let i = 0;  i <= 19;  i++) {
+        let url = getPref(`floorp.browser.sidebar2.customurl${i}`);
+        let usercontext = getPref(`floorp.browser.sidebar2.customurl${i}.usercontext`);
+        let width = getPref(`floorp.browser.sidebar2.width.mode${i}`);
+
+        let willMigrate = true;
+        if (url === undefined) willMigrate = false;
+        for (let key of Object.keys(init_data["data"])) {
+            if (init_data["data"][key]["url"] === url) {
+                willMigrate = false;
+            }
+        }
+        if (!willMigrate) continue;
+
+        let newPanelId = `w${String((new Date()).getTime())}`;
+        let newdata = {
+            "url": url,
+            "usercontext": usercontext !== undefined ?
+                            String(usercontext) :
+                            null,
+            "userAgent": false,
+            "width": width !== undefined ?
+                      Number(width) :
+                      0,
+        }
+
+        init_data["data"][newPanelId] = newdata;
+    }
+    Services.prefs.setStringPref(BROWSER_SIDEBAR_DATA_PREF, JSON.stringify(init_data));
+
+    const v2_example = {
+        "data": {
+            "w20221023122919": {
+                "url": "https://open.spotify.com",
+                "usercontext": "1",
+                "userAgent": false,
+                "width": 770
+            }
+        },
+        "index": [
+            "1"
+        ]
+    }
+
+    BROWSER_SIDEBAR_DATA = JSON.parse(Services.prefs.getStringPref(BROWSER_SIDEBAR_DATA_PREF, "{}"));
+    BROWSER_SIDEBAR_IDS = BROWSER_SIDEBAR_DATA["index"];
+}
+
+const BMSManager = {
+    CURRENT_PANEL: null,
+
+    isInitialized: function() {
+        return BROWSER_SIDEBAR_DATA !== null;
+    },
+    addPanelWithPrompt: function(url, usercontext){
+        if (!this.isInitialized) throw "Not initialized!";
+        let parentWindow = window;
+        let newPanelId = `w${String((new Date()).getTime())}`;
+        let panelInfo = {
+            "new": true,
+            "id": newPanelId,
+            "url": url ? url : null,
+            "userContext": usercontext ? usercontext : null,
+        };
+        if (
+            parentWindow?.document.documentURI ==
+            "chrome://browser/content/hiddenWindowMac.xhtml"
+        ) {
+            parentWindow = null;
+        }
+        if (parentWindow?.gDialogBox) {
+            parentWindow.gDialogBox.open(
+                "chrome://browser/content/preferences/dialogs/customURLs.xhtml",
+                object
+            );
+        } else {
+            Services.ww.openWindow(
+                parentWindow,
+                "chrome://browser/content/preferences/dialogs/customURLs.xhtml",
+                "AddWebpanel",
+                "chrome,titlebar,dialog,centerscreen,modal",
+                panelInfo
+            );
+        }
+    },
+    addPanel: function(url, usercontext){
+        if (!this.isInitialized) throw "Not initialized!";
+        let newPanelId = `w${String((new Date()).getTime())}`;
+        BROWSER_SIDEBAR_DATA["data"][newPanelId] = {
+            "url": url,
+            "usercontext": String(usercontext),
+            "userAgent": false,
+            "width": 0,
+        };
+        BROWSER_SIDEBAR_DATA["index"].push(newPanelId);
+        Services.prefs.setStringPref(BROWSER_SIDEBAR_DATA_PREF, JSON.stringify(BROWSER_SIDEBAR_DATA));
+    },
+    removePanel: function(id){
+        if (!this.isInitialized) throw "Not initialized!";
+        delete BROWSER_SIDEBAR_DATA["data"][id];
+        BROWSER_SIDEBAR_DATA["index"] = 
+            BROWSER_SIDEBAR_DATA["index"].filter(index => index !== id);
+        Services.prefs.setStringPref(BROWSER_SIDEBAR_DATA_PREF, JSON.stringify(BROWSER_SIDEBAR_DATA));
+        document.getElementById(`webpanel_${id}`)?.remove();
+    },
+    movePanel: function(from, to){
+        if (!this.isInitialized) throw "Not initialized!";
+        let from_id = BROWSER_SIDEBAR_IDS[from];
+        delete BROWSER_SIDEBAR_DATA["index"][from];
+        BROWSER_SIDEBAR_DATA["index"][to] = from_id;
+        Services.prefs.setStringPref(BROWSER_SIDEBAR_DATA_PREF, JSON.stringify(BROWSER_SIDEBAR_DATA));
+    },
+    editPanel: function(id, panelData){
+        if (!this.isInitialized) throw "Not initialized!";
+        BROWSER_SIDEBAR_DATA["data"][id] = Object.assign(BROWSER_SIDEBAR_DATA["data"][id], panelData);
+        Services.prefs.setStringPref(BROWSER_SIDEBAR_DATA_PREF, JSON.stringify(BROWSER_SIDEBAR_DATA));
+        if (panelData["url"] !== undefined ||
+            panelData["userAgent"] !== undefined ||
+            panelData["usercontext"] !== undefined
+        ) {
+            document.getElementById(`webpanel_${id}`)?.remove();
+        }
+        if (this.CURRENT_PANEL === id) {
+            this.show(id);
+        }
+    },
+    show: function(id){
+        if (!this.isInitialized) throw "Not initialized!";
+        let panelData = BROWSER_SIDEBAR_DATA["data"][id];
+        let panelURL = panelData["url"];
+        let panelSrc = panelURL;
+        let privileged = false;
+        if (STATIC_SIDEBARS[panelURL] !== undefined) {
+            panelSrc = STATIC_SIDEBARS[panelURL]["src"];
+            privileged = Boolean(STATIC_SIDEBARS[panelURL]["privileged"]);
+        }
+
+        for (let browser of document.querySelectorAll("#sidebar2-box > browser")) {
+            browser.setAttribute("hidden", "true");
+        }
+
+        let browserManagerSidebarWebpanel = document.getElementById(`webpanel_${id}`);
+        if (browserManagerSidebarWebpanel !== null) {
+            // show
+            browserManagerSidebarWebpanel.removeAttribute("hidden");
+        } else {
+            browserManagerSidebarWebpanel = document.createXULElement("browser");
+
+            const SHARED_ATTRIBUTES = {
+                "xmlns": "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+                "id": `webpanel_${id}`,
+                "class": "webpanels",
+                "flex": "1",
+                "autoscroll": "false",
+                "disablehistory": "true",
+                "tooltip": "aHTMLTooltip",
+            }
+            const PRIVILEGED_ATTRIBUTES = {
+                "disablefullscreen": "true",
+                "disableglobalhistory": "true",
+                "messagemanagergroup": "webext-browsers",
+                "context": "",
+                "webextension-view-type": "sidebar",
+                "autocompletepopup": "PopupAutoComplete",
+                "initialBrowsingContextGroupId": "40",
+                "type": "content",
+                /*
+                "remote": "true",
+                */
+                "maychangeremoteness": "true",
+                "src": panelSrc,
+            }
+            const NOT_PRIVILEGED_ATTRIBUTES = {
+                "disablefullscreen": "true",
+                "disableglobalhistory": "true",
+                "messagemanagergroup": "webext-browsers",
+                "context": "",
+                "webextension-view-type": "sidebar",
+                "autocompletepopup": "PopupAutoComplete",
+                "initialBrowsingContextGroupId": "40",
+                "type": "content",
+                "remote": "true",
+                "maychangeremoteness": "true",
+                "src": panelSrc,
+            }
+
+            for (let key of Object.keys(SHARED_ATTRIBUTES)) {
+                browserManagerSidebarWebpanel.setAttribute(key, SHARED_ATTRIBUTES[key]);
+            }
+            if (privileged) {
+                for (let key of Object.keys(PRIVILEGED_ATTRIBUTES)) {
+                    browserManagerSidebarWebpanel.setAttribute(key, PRIVILEGED_ATTRIBUTES[key]);
+                }
+            } else {
+                for (let key of Object.keys(NOT_PRIVILEGED_ATTRIBUTES)) {
+                    browserManagerSidebarWebpanel.setAttribute(key, NOT_PRIVILEGED_ATTRIBUTES[key]);
+                }
+            }
+
+            document.getElementById("sidebar2-box").appendChild(browserManagerSidebarWebpanel);
+
+            browserManagerSidebarWebpanel.removeAttribute("hidden");
+        }
+
+        browserManagerSidebarWebpanel.setAttribute("width", panelData["width"]);
+
+        this.CURRENT_PANEL = id;
+    },
+    hidden: function(id, background = true){
+        if (!this.isInitialized) throw "Not initialized!";
+        for (let browser of document.querySelectorAll("#sidebar2-box > browser")) {
+            browser.setAttribute("hidden", "true");
+        }
+        if (id !== null && !background) {
+            document.getElementById(`webpanel_${id}`)?.remove();
+        }
+        this.CURRENT_PANEL = null;
+    }
+};
+
+// init
+(async() => {
+    if (!Services.prefs.getBoolPref("floorp.browser.sidebar.enable", false)) return;
+
+    let os_languages = Cc["@mozilla.org/intl/ospreferences;1"].getService(Ci.mozIOSPreferences).regionalPrefsLocales;
+    let prefBranch = Services.prefs.getDefaultBranch(null);
+    prefBranch.setStringPref(
+    "floorp.browser.sidebar.useIconProvider",
+    os_languages.includes("zh-CN") ?
+        "yandex" :
+        "duckduckgo"
+    );
+
+    readBMSData();
+
+    Services.prefs.addObserver(BROWSER_SIDEBAR_DATA_PREF, function() {
+        readBMSData();
+    });
+})();
